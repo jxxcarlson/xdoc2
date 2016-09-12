ddaf8a248b08a180207203fa35070b971eb7017b
James Carlson <jxxcarlson@gmail.com>
2016-09-12 08:52:48 -0400

    In delete document controller, handle cases of hard, soft deletes and undelete

--- apps/api/controllers/documents/delete.rb
+++ apps/api/controllers/documents/delete.rb
@@ -7,24 +7,37 @@ module Api::Controllers::Documents
 
     include Permission
 
+    def reply(mode, id)
+      case mode
+        when 'soft'
+          { 'status': 'success','info': "document #{id} deleted (soft)" }
+        when 'hard'
+          { 'status': 'success','info': "document #{id} deleted (hard)" }
+        when 'undelete'
+          { 'status': 'success','info': "document #{id} undeleted" }
+      end
+    end
+
     def delete_document(params)
       id = params['id']
       document = DocumentRepository.find(id)
       command = request.query_string
+      _verb, mode = command.split('=')
 
       if document && document.owner_id == @access.user_id
-        case command
+        case mode
           when 'soft'
+            puts "Soft delete document #{id}"
             document.set_status('deleted')
+            document.unlink
           when 'hard'
+            puts "Hard delete document #{id}"
             NSDocument.delete document
           when 'undelete'
+            puts "Undeleting document #{id}"
             document.set_status('')
-          else
-            document.set_status('deleted')
         end
-        puts "Document #{id} deleted"
-        reply =  { 'status': 'success','info': "document{#id} deleted" }
+        reply =  reply(mode, id)
       else
         self.body = { "error" => "500 Server error: document not found or processed or permissions invalid" }.to_json
       end
@@ -34,9 +47,17 @@ module Api::Controllers::Documents
     def call(params)
 
       verify_request(request)
+      puts "@access.valid: #{@access.valid}"
+      puts "@access.username: #{@access.username}"
 
       document = DocumentRepository.find params['id']
+
       author = UserRepository.find_by_username @access.username
+
+      puts "author.id: #{author.id}"
+      puts "document.owner_id: #{document.owner_id}"
+
+
 
       if @access.valid  && author.id == document.owner_id
         delete_document(params)
